b07e105a15d28d3f398b279a5bdc0e81bd411fb5
abhikhur_dev <abhimanyukhurana007@gmail.com>
2024-04-22 20:40:21 -0500

    idk heroku is being dumb

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
